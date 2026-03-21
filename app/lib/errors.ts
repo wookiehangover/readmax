63b9e6c39e2dfc58cfec8ec5d2253816f0c4f94c
Sam Breed <samuel.breed@gmail.com>
2026-03-20 21:18:08 -0700

    feat: add typed error classes using Effect library
    Agent-Id: agent-5bd1dc17-8b4b-4fa2-8f84-8065b160614f
    Linked-Note-Id: 5ebab3a9-ea6c-453e-904e-dd77ab85a149

---
+++ app/lib/errors.ts
@@ -0,0 +1,33 @@
+import { Data } from "effect";
+
+export class StorageError extends Data.TaggedError("StorageError")<{
+  readonly operation: string;
+  readonly cause?: unknown;
+}> {}
+
+export class BookNotFoundError extends Data.TaggedError("BookNotFoundError")<{
+  readonly bookId: string;
+}> {}
+
+export class EpubParseError extends Data.TaggedError("EpubParseError")<{
+  readonly cause?: unknown;
+}> {}
+
+export class HighlightError extends Data.TaggedError("HighlightError")<{
+  readonly operation: string;
+  readonly highlightId?: string;
+  readonly cause?: unknown;
+}> {}
+
+export class NotebookError extends Data.TaggedError("NotebookError")<{
+  readonly operation: string;
+  readonly bookId?: string;
+  readonly cause?: unknown;
+}> {}
+
+export class PositionError extends Data.TaggedError("PositionError")<{
+  readonly operation: string;
+  readonly bookId: string;
+  readonly cause?: unknown;
+}> {}
+
